15610a743764cf017f760dbc4556118c936011d9
Vitor Duarte <65680799+vitordbo@users.noreply.github.com>
2023-11-01 21:05:11 -0300

    home view and friends view

--- FoodApp/FoodApp/Views/FriendsView.swift
+++ FoodApp/FoodApp/Views/FriendsView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct FriendsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack {
+                ScrollView {
+                    Text("Veja o que seus amigos estao fazendo").font(.title).fontWeight(.bold)
+                    
+                    // colocar em um for each para aparecer varias diferentes
+                    // colocar scroll view tbm (talvez no vStack)
+                    HStack {
+                        Image("lasanha").scaledToFit().frame(width: 220, height: 120).scaleEffect(0.25)
+                        Spacer()
+                        VStack{
+                            Text("Vitor")
+                            Text("Receita top 1")
+                            Text("⭐️⭐️⭐️⭐️⭐️")
+                        }.padding(.trailing, 30)
+                    }.padding(.bottom, 20)
+                    
+                    HStack { // colocar em um for each
+                        Image("fricasse").scaledToFit().frame(width: 220, height: 120).scaleEffect(0.2)
+                        Spacer()
+                        VStack{
+                            Text("Ricfardo")
+                            Text("Receita top 1")
+                            Text("⭐️⭐️⭐️⭐️⭐️")
+                        }.padding(.trailing, 30)
+                    }
+                }
+            }.padding(.top, 10)
+        }
     }
 }
 
--- FoodApp/FoodApp/Views/HomeView.swift
+++ FoodApp/FoodApp/Views/HomeView.swift
@@ -18,34 +18,37 @@ struct HomeView: View {
                       .frame(height: 200)
                       .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
               }
+              Spacer()
           }
-            
-            VStack {
-                Text("Bem-vindo").padding(.top, 40)
-                Text("Isso foi oq vc viu recentemente")
-                
-                // colocar em um for each para aparecer varias diferentes
-                // colocar scroll view tbm (talvez no vStack)
-                HStack {
-                    Image("lasanha").scaledToFit().frame(width: 220, height: 120).scaleEffect(0.25)
-                    Spacer()
-                    VStack{
-                        Text("Receita top 1")
-                        Text("Tempo 15")
-                        Text("⭐️⭐️⭐️⭐️⭐️")
-                    }.padding(.trailing, 30)
-                }.padding(.bottom, 20)
-                
-                HStack { // colocar em um for each
-                    Image("lasanha").scaledToFit().frame(width: 220, height: 120).scaleEffect(0.25)
-                    Spacer()
-                    VStack{
-                        Text("Receita top 1")
-                        Text("Tempo 15")
-                        Text("⭐️⭐️⭐️⭐️⭐️")
-                    }.padding(.trailing, 30)
-                }
-            }.padding(.top, 120)
+     
+            ScrollView {
+                VStack {
+                    Text("Bem-vindo").padding(.top, 40)
+                    Text("Isso foi oq vc viu recentemente")
+                    
+                    // colocar em um for each para aparecer varias diferentes
+                    // colocar scroll view tbm (talvez no vStack)
+                    HStack {
+                        Image("lasanha").scaledToFit().frame(width: 220, height: 120).scaleEffect(0.25)
+                        Spacer()
+                        VStack{
+                            Text("Receita top 1")
+                            Text("Tempo 15")
+                            Text("⭐️⭐️⭐️⭐️⭐️")
+                        }.padding(.trailing, 30)
+                    }.padding(.bottom, 20)
+                    
+                    HStack { // colocar em um for each
+                        Image("lasanha").scaledToFit().frame(width: 220, height: 120).scaleEffect(0.25)
+                        Spacer()
+                        VStack{
+                            Text("Receita top 1")
+                            Text("Tempo 15")
+                            Text("⭐️⭐️⭐️⭐️⭐️")
+                        }.padding(.trailing, 30)
+                    }
+                }.padding(.top, 200)
+            }
       }
     }
 }
