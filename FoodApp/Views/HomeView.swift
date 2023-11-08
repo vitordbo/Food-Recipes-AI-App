54e68d9ac9d414827837b4c634a78c93f7b2a7f9
Vitor Duarte <65680799+vitordbo@users.noreply.github.com>
2023-11-08 19:05:53 -0300

    att

--- FoodApp/Views/HomeView.swift
+++ FoodApp/Views/HomeView.swift
@@ -10,52 +10,54 @@ import SwiftUI
 struct HomeView: View {
     @State private var searchText = ""
     @State private var isButton = false
-
+    
     var body: some View {
-       
+        
         ZStack{
             //Color(.red).opacity(0.7).ignoresSafeArea()
             ScrollView {
                 
-            VStack {
+                VStack {
                     
-                HStack{
-                    TextField( "Pesquise qualquer receita",text: $searchText).multilineTextAlignment(.center)
-                        .font(.title3)
-                        .padding(.horizontal,10)
-                        .textFieldStyle(.roundedBorder)
-                        .frame(height: 10)
-                        .padding(.top,20)
-                    Button(
-                        action: {
-                            isButton = true
-                        }, label: {
-                            HStack{
-                                Image(systemName: "magnifyingglass")
-                            }.padding()
-                                .foregroundColor(.white)
-                                .background(.yellow)
-                                .cornerRadius(10)
-                                .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 10))
-                        }).navigationDestination(isPresented: $isButton){
-                            RecipeSearchView(queryFromHome: searchText)
-                            Text("").hidden()
+                    HStack{
+                        TextField( "Pesquise qualquer receita",text: $searchText).multilineTextAlignment(.center)
+                            .font(.title3)
+                            .padding(.horizontal,10)
+                            .textFieldStyle(.roundedBorder)
+                            .frame(height: 10)
+                            .padding(.top,20)
+                        Button(
+                            action: {
+                                isButton = true
+                            }, label: {
+                                HStack{
+                                    Image(systemName: "magnifyingglass")
+                                }.padding()
+                                    .foregroundColor(.white)
+                                    .background(.yellow)
+                                    .cornerRadius(10)
+                                    .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 10))
+                            }).navigationDestination(isPresented: $isButton){
+                                RecipeSearchView(queryFromHome: searchText)
+                                Text("").hidden()
+                            }
+                    }
+                    
+                    
+                    
+                    NavigationView {
+                        List {
+                            ImageSlider()
+                                .frame(height: 200)
+                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                         }
-                }
+                        
+                    }.padding(.top,20)
+                        .navigationBarBackButtonHidden(true)
+                        .navigationBarHidden(true)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: -350, trailing: 0))
                     
-              NavigationView {
-                  List {
-                      ImageSlider()
-                          .frame(height: 200)
-                          .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                  }
-                  
-              }.padding(.top,20)
-              .navigationBarBackButtonHidden(true)
-              .navigationBarHidden(true)
-              .padding(EdgeInsets(top: 0, leading: 0, bottom: -350, trailing: 0))
-     
-            
+                    
                     Text("Bem-vindo")
                     Text("Isso foi oq vc viu recentemente")
                     
@@ -85,10 +87,10 @@ struct HomeView: View {
         }
     }
 }
-    
-    struct HomeView_Previews: PreviewProvider {
-        static var previews: some View {
-            HomeView()
-        }
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
     }
+}
 
---
+++ FoodApp/Views/nodeRedApi.swift
@@ -0,0 +1,58 @@
+
+import Foundation
+
+ struct User {
+     static var usuario: Pessoa = Pessoa(email: "")
+}
+
+struct Pessoa : Decodable, Hashable {
+    let email: String?
+}
+
+struct Compras : Decodable, Hashable {
+    let name: String?
+    let quantity: Int?
+    let isChecked: Bool?
+}
+
+class MVShopList : ObservableObject {
+    
+    @Published var lista: [Pessoa] = []
+    
+    func fetch(email: String){
+        guard let url = URL(string: "http://127.0.0.1:1880/api/v1/findAll") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){ [weak self] data, _, error in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do{
+                let parsed = try JSONDecoder().decode([Pessoa].self, from: data)
+                DispatchQueue.main.async {
+                    self?.lista = parsed
+                    self?.findUser(email: email)
+                    print(parsed)
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    func findUser(email: String) {
+        for pessoa in lista {
+            if pessoa.email == email {
+                User.usuario = pessoa
+                print(User.usuario.email!)
+            }
+        }
+    }
+}
