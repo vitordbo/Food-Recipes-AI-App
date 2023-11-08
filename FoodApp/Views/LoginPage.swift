5a07ae06ce221f7099eb611eef5dce2ceb8511ce
Ricardo Junior <89224705+RicardoJun10r@users.noreply.github.com>
2023-11-07 21:44:21 -0300

    Update LoginPage.swift

--- FoodApp/Views/LoginPage.swift
+++ FoodApp/Views/LoginPage.swift
@@ -4,51 +4,39 @@ struct LoginPage: View {
     @State private var username: String = ""
     @State private var password: String = ""
     @State private var isButton = false
+    @StateObject var API = MVShopList()
     
     var body: some View {
             ZStack{
-                Color(.red).opacity(0.7).ignoresSafeArea(.all)
+                Color(.red).opacity(0.7).ignoresSafeArea()
                 VStack{
-                    Image("Image")
-                        .resizable()
-                        .frame(width: 150,height: 150)
-                        .scaleEffect(1)
+                    Image("Image").resizable().frame(width: 150,height: 150).scaleEffect(1)
+                    Text("Login").font(.title).foregroundColor(.white).fontWeight(.bold)
                     
-                    Text("Login").font(.title).foregroundColor(.white)
-                        .fontWeight(.bold)
-                    
-                    TextField("Email", text: $username).font(.title3)
-                        .padding(.horizontal,30)
+                    TextField("Email", text: $username).font(.title3).padding(.horizontal,30)
                         .textFieldStyle(.roundedBorder)
-                        .frame(height: 10)
-                        
                     
-                    SecureField("Senha", text: $password)
-                        .font(.title3)
-                        .padding(.horizontal,30)
+                    TextField("Senha", text: $password).font(.title3).padding(.horizontal,30)
                         .textFieldStyle(.roundedBorder)
-                        .frame(height: 10)
-                        .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
                     
-                    Button(
-                        action: {
-                                            isButton = true
-                                        }, label: {
-                                            HStack{
-                                                Text("Entrar").fontWeight(.bold).font(.title3)
-                                            }.padding()
-                                                .foregroundColor(.white)
-                                                .background(.yellow)
-                                                .cornerRadius(10)
-                                                .padding(.top,10)
-                                        }).navigationDestination(isPresented: $isButton){
-                                            TabViewHome()
-                                            Text("").hidden()
-                                        }
+                    Button(action: {
+                        API.fetch(email: username)
+                        isButton = true
+                    }, label: {
+                        HStack{
+                            Text("Entrar").fontWeight(.bold).font(.title3)
+                        }.padding()
+                            .foregroundColor(.white)
+                            .background(.yellow)
+                            .cornerRadius(10)
+                            .padding(.top,10)
+                    }).navigationDestination(isPresented: $isButton){
+                        TabViewHome()
+                        Text("")
+                            .hidden()
+                    }
                 }
             }
-            .navigationBarBackButtonHidden(true)
-            .navigationBarHidden(true)
         }
 }
 
@@ -57,4 +45,3 @@ struct LoginPage_Previews: PreviewProvider {
         LoginPage()
     }
 }
-
